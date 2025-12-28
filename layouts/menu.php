579393353227c9a9cc14d775b66bed25edc44132
CitrusTime <ajanidbrown96@gmail.com>
2025-12-28 00:39:51 -0500

    Previous file I just accidently deleted them...

---
+++ layouts/menu.php
@@ -0,0 +1,12 @@
+<nav>
+
+    <ul>
+
+        <li><a href="dashboard.php">Home</a></li>
+        <li><a href="new_contact.php">New Contact</a></li>
+        <li><a href="user_list.php">Users</a></li>
+        <li><a href="#">Logout</a></li>
+
+    </ul>
+
+</nav>
